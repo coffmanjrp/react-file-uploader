0f795b41c93fbe52f04ccf86d6bf352ebd417685
coffmanjrp <coffmanjrp@gmail.com>
2021-02-02 05:22:30 -0800

    change App.js to jsx and set base styles

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mt-4">
+      <h1 className="display-4 text-center mb-4">
+        <i className="fab fa-react"></i> React File Upload
+      </h1>
+    </div>
+  );
+}
+
+export default App;
