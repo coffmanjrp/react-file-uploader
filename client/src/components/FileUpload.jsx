3f3e3381c17777c1f2fad9b18d0d1796921c20fd
coffmanjrp <coffmanjrp@gmail.com>
2021-02-02 05:52:59 -0800

    add image upload function and upload image to upload folder

--- client/src/components/FileUpload.jsx
+++ client/src/components/FileUpload.jsx
@@ -1,11 +1,51 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 const FileUpload = () => {
+  const [file, setFile] = useState('');
+  const [fileName, setFileName] = useState('Choose File');
+  const [uploadedFile, setUploadedFile] = useState({});
+
+  const onChange = (e) => {
+    setFile(e.target.files[0]);
+    setFileName(e.target.files[0].name);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const res = await axios.post('/upload', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+
+      const { fileName, filePath } = res.data;
+
+      setUploadedFile({ fileName, filePath });
+    } catch (err) {
+      if (err.response.status === 500) {
+        console.log('There was a problem with the server');
+      } else {
+        console.log(err.response.data.msg);
+      }
+    }
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="custom-file mt-4">
-          <input type="file" className="custom-file-input" id="customFile" />
+          <input
+            type="file"
+            className="custom-file-input"
+            id="customFile"
+            onChange={onChange}
+          />
           <label className="custom-file-label" htmlFor="customFile">
-            Choose file
+            {fileName}
           </label>
           <input
             type="submit"
